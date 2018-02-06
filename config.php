d2dcf2d3d7c2be8a0638f116621f8c93dc667071
PHPMySQLbasics <36168164+PHPMySQLbasics@users.noreply.github.com>
2018-02-06 15:30:05 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,23 +1,25 @@
 <?php
+/**
+ * opens a connection to the mysql db
+ * if a exception will be thrown the message will be displayed
+ */
 
 $server = 'localhost';
 $user = 'root';
 $pwd = '';
-$db = 'mydb';
-
+$db = 'theater';
 try
 {
-    $con = new PDO('mysql:host='.$server.
-        ';dbname='.$db.';charset=utf8', $user, $pwd);
-} catch(Exception $e)
+    $con = new PDO('mysql:host='.$server.';dbname='.$db.';charset=utf8',$user, $pwd);
+}
+catch (Exception $e)
 {
     switch($e->getCode())
     {
         case 1049:
-            echo 'Datenbank '.$db.' nicht vorhanden!<br>';
+            echo 'Datenbank ' .$db.' nicht vorhanden!';
             break;
         default:
             echo $e->getMessage();
     }
-
 }
--- index.php
+++ index.php
@@ -1,33 +1,45 @@
+<?php
+/**
+ * Sample index.php with a navigation for different pages
+ */
+?>
+
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>Filmdatenbank</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <title>Theater</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> <!-- include bootstrap -->
 </head>
 <body>
 <nav>
+    <!-- include the navigation bar -->
     <?php
     include 'nav.html';
     ?>
 </nav>
-<main>
-    <br>
-<?php
-include 'config.php';
+<div class="container">
+    <main>
 
-    switch($_GET['seite'])
-    {
-        case 'suche_produktionsfirma':
-            include 'sucheProduktionsfirma.php';
-            break;
-        case 'suche_schauspieler':
-            include 'sucheschauspieler.php';
-            break;
-        default:
-            break;
-    }
-?>
-</main>
+        <!-- extends the url with the selected page and include the selected php file -->
+        <?php
+        if (isset($_GET['seite']))
+        {
+            switch($_GET['seite'])
+            {
+                case 'startseite':
+                    include 'start.php';
+                    break;
+                case 'neuspielplan':
+                    include 'neuspielplan.php';
+                    break;
+                case 'suchetheaterstuck':
+                    include 'suchetheaterstuck.php';
+                    break;
+            }
+        }
+        ?>
+    </main>
+</div>
 </body>
 </html>
